19cfe27a94adfaed463b9bffc0e8e477d3fce5d5
Piotr Suchy <piotrsuchy6@tlen.pl>
2022-10-06 18:49:58 +0200

    test

---
+++ zadanie_3.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+
+def function(x):
+    return x*x + 5
+
+def main():
+    print(function(2))
+
+if __name__ == "__main__":
+    main()
